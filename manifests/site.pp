0d4ad1666e234568c541b67c2c843703e6ce093e
fred <fred@happylatte.com>
2014-06-18 18:40:47 +0800

    Setup firewall.

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,7 @@
 class base {
   include system
   include '::ntp'
+  #class { ['my_fw::pre', 'my_fw::post']: }
 }
 
 import 'nodes/*.pp'
@@ -9,3 +10,48 @@ node default {
   include base
 }
 
+## firewall
+resources { "firewall":
+  purge => true
+}
+Firewall {
+  before  => Class['my_fw::post'],
+  require => Class['my_fw::pre'],
+}
+
+class my_fw::pre {
+  Firewall {
+    require => undef,
+  }
+
+  # Default firewall rules
+  firewall { '000 drop all icmp':
+    proto   => 'icmp',
+    action  => 'drop',
+  }->
+  firewall { '001 accept all to lo interface':
+    proto   => 'all',
+    iniface => 'lo',
+    action  => 'accept',
+  }->
+  firewall { '002 accept related established rules':
+    proto   => 'all',
+    ctstate => ['RELATED', 'ESTABLISHED'],
+    action  => 'accept',
+  }
+
+  firewall { '003 allow ssh access':
+    port   => [22],
+    proto  => tcp,
+    action => accept,
+  }
+}
+
+class my_fw::post {
+  firewall { '999 drop all':
+    proto   => 'all',
+    action  => 'drop',
+    before  => undef,
+  }
+}
+## end of firewall
