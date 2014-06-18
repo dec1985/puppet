f5f6e9540717fb2b4c4a2dc19422d3af2dc0ec8a
fred <fred@happylatte.com>
2014-06-19 07:39:54 +0800

    Add some modules.

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,74 @@
+class postgres {
+  class { 'postgresql::server':
+    ip_mask_deny_postgres_user => '0.0.0.0/32',
+    ip_mask_allow_all_users => '10.171.22.0/8',
+    listen_addresses => '*',
+    postgres_password => 'TPSrep0rt!',
+  }
+}
+
+class application_base {
+  # Note: for debian serious only
+  exec { 'apt-update':
+    command => '/usr/bin/apt-get update',
+    onlyif => "/bin/bash -c 'exit $(( $(( $(date +%s) - $(stat -c %Y /var/lib/apt/lists/$( ls /var/lib/apt/lists/ -tr1|tail -1 )) )) <= 604800 ))'"
+  }->
+  package { [
+             'libgeoip-dev',
+             'libzmq-dev',
+             'swig',
+             'make',
+             # already installed on ubuntu12.04 directly
+             'python2.7',
+             # Class['python'] will install python-dev, is it enough?
+             'python2.7-dev',
+             # conflict with Class['python']
+             #'python-virtualenv'
+             ]:
+    ensure => present,
+  }
+}
+
+class my_python {
+  #vcsrepo { '/tmp/vcstest-git-clone':
+  #  ensure   => present,
+  #  provider => git,
+  #  source   => 'git://github.com/bruce/rtex.git',
+  #}
+  class { 'python':
+    pip => true,
+    dev => true,
+    virtualenv => true,
+  }
+  python::virtualenv { '/opt/env/redisenv':
+    ensure => present,
+    version => '2.7',
+    #requirements => '',
+    distribute => false,
+  }
+}
+
 class base {
   include system
   include '::ntp'
+  include git
   #class { ['my_fw::pre', 'my_fw::post']: }
 }
 
 import 'nodes/*.pp'
+
+node 'puppet-slave1' {
+  include base
+
+  include postgres
+
+  class { 'redis':
+    version => '2.6.7',
+  }
+
+  include application_base
+  include my_python
+}
 
 node default {
   include base
