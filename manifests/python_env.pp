fc164830050592f304b3a2c0d01d112e48b14ae6
fred <fred@happylatte.com>
2014-06-19 17:21:55 +0800

    Fix package errors.
    Reason:
     * Pip(since 1.4) won't install unstable version by default, unless you specify it forcely.
     * pip will download necessary pakcages and install some of them in a bunch.

--- manifests/python_env.pp
+++ manifests/python_env.pp
@@ -39,7 +39,7 @@ class python_env {
     environment => 'PIP_PYPI_URL=https://highnoon:JstSmthngNwO_O@pypi.happylatte.com/private/',
   }
 
-  # hns - for monitor
+  # sso monitor
   python::virtualenv { '/opt/env/hnsenv':
     ensure => present,
     version => '2.7',
@@ -49,6 +49,7 @@ class python_env {
     virtualenv => '/opt/env/hnsenv',
     environment => 'PIP_PYPI_URL=https://highnoon:JstSmthngNwO_O@pypi.happylatte.com/private/',
   }
+
   # hns
   file { '/home/highnoon/log':
     ensure => directory,
@@ -58,70 +59,20 @@ class python_env {
   python::virtualenv { '/home/highnoon/hnenv':
     ensure => present,
     version => '2.7',
-    systempkgs => false,
-    distribute => true,
+    distribute => false,
     owner => 'highnoon',
     group => 'highnoon',
-    timeout => 0,
   }->
-  python::pip { ['argparse==1.2.1',
-                 'texttable==0.8.1',
-                 'pytz==2012c',
-                 'biplist==0.4',
-                 'ujson==1.18',
-                 'dnspython==1.10.0',
-                 'redis==2.2.4',
-                 'pycrypto==2.4.1',
-                 'pyasn1==0.0.13',
-                 'greenlet==0.4.0',
-                 'pyOpenSSL==0.13',
-                 'guppy==0.1.9',
-                 'eventlet==0.9.16',
-                 'configobj==4.7.2',
-                 'Jinja2==2.1.1',
-                 'mock==0.6.0',
-                 'psycopg2==2.4.5',
-                 'simplejson==2.0.9',
-                 'pyzmq==2.2.0',
-                 'zope.interface==4.0.1',
-                 'Twisted==12.1.0',
-                 'txZMQ==0.6.1',
-                 'pyYAML==3.10',
-                 'protobuf==2.4.1',
-                 'pycountry==1.4',
-                 'geoip2==0.5.1',
-                 'pyfacebook-happylatte==1.0a2',
-                 'Thrift-happylatte-py==0.8.0',
-                 'sallyconf==1.1.0',
-                 'hnProtocol==1.4.0.4',
-                 'hnYellow==2.4.2',
-                 'redisd==0.3.0',
-                 'hnRedis==0.3.8',
-                 'hnMonitor==1.0.4',
-                 'hnDB==1.2.2',
-                 'sallyutils==0.16.15',
-                 'sallylog==0.3.3',
-                 'hnHook==1.2.12',
-                 'hndao==0.5.7',
-                 'hnStrings==1.0.7',
-                 'hnLadder==0.5.30.7',
-                 'hnAPN==1.1.5',
-                 'hnTel==0.3.11',
-                 'hnJournal==0.4.5',
-                 'hnMunin==0.5.10',
-                 'hnStats==0.1.12',
-                 'etYellowUtils==0.5.4-r2',
-                 'notification==0.3.5',
-                 'M2Crypto-happylatte==0.22r739.dev-r739',
-                 'hnCompetition==0.2.29',
-                 'hnItems==1.0.1',
-                 'chatlib==0.3.18',
-                 'happybroker==0.6.30',
-                 'hnCasino==0.10.6',
-                 'hnMission==0.0.45.32',
-                 'statsproto==0.0.32']:
-    virtualenv => '/home/highnoon/hnenv',
-    owner => 'highnoon',
+  file { '/etc/requirements.txt':
+    ensure => present,
+    source => 'puppet:///extra_files/requirements.txt',
+  }~>
+  exec { 'setup.sh':
+    command => 'pip install -r /etc/requirements.txt > /tmp/setup.sh.log',
+    user => 'highnoon',
+    group => 'highnoon',
+    path => ['/home/highnoon/hnenv/bin/', '/usr/local/bin', '/usr/bin', '/bin'],
+    cwd => '/home/highnoon',
     environment => ['PIP_PYPI_URL=https://highnoon:JstSmthngNwO_O@pypi.happylatte.com/private/',
                     'ARCHFLAGS="-arch i386 -arch x86_64"'],
   }
