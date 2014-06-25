1fa4b9b7f68e13bda6817970bb666277b45052ad
fred <fred@happylatte.com>
2014-06-25 10:29:44 +0000

    Try to use module as much as possible.

--- manifests/base.pp
+++ manifests/base.pp
@@ -2,13 +2,13 @@ class base {
   include system
   include ntp
   include motd
-  include oak
 
-  # TODO: for debian os only.
-  exec { 'apt-update':
-    command => '/usr/bin/apt-get update',
-    onlyif  => "/bin/bash -c 'exit $(( $(( $(date +%s) - $(stat -c %Y /var/lib/apt/lists/$( ls /var/lib/apt/lists/ -tr1|tail -1 )) )) <= 604800 ))'"
-  } ->
-  package { ['htop', 'dstat', 'iotop', 'tree']: ensure => present, }
+  if $::osfamily == 'Debian' {
+    include apt
+  }
   include git
+
+  package { ['htop', 'dstat', 'iotop', 'tree']: ensure => present, }
+  # fred's module
+  include oak
 }
